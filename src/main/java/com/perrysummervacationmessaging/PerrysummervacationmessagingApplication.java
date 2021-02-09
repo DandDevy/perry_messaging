7857164012dd8e0d042a4cb1f8365af95fd78d67
Daniel Ashcroft <dashcroft4@gmail.com>
2021-02-09 17:54:29 +0000

    Add initialisation of project

---
+++ src/main/java/com/perrysummervacationmessaging/PerrysummervacationmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.perrysummervacationmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerrysummervacationmessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerrysummervacationmessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perrysummervacationmessaging/PerrysummervacationmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perrysummervacationmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerrysummervacationmessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
